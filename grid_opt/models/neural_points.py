0e4aef01ad332c36cf5adc360c8ebc12ab692f79
Mathias Otnes <Mathias.otnes@gmail.com>
2026-02-17 14:49:07 -0800

    Config update

--- grid_opt/models/neural_points.py
+++ grid_opt/models/neural_points.py
@@ -349,9 +349,6 @@ class NeuralPoints(BaseNet):
         
         N = x.shape[0]
         
-        # Overriding these for test purposes FIXME
-        K = 15 
-        
         # Neighbor lookup
         Np_idx = self.query_neighbors(x, K=K)
         valid = Np_idx >= 0 # (N,K) bool mask for valid neighbors
